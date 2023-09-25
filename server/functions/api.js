14e54703275ab95eb13e168c5e92104f7578a884
Stefan Turner <turner.stefan281@gmail.com>
2023-09-25 15:44:07 +0200

    add get scores endpoint

--- server/functions/api.js
+++ server/functions/api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const helmet = require("helmet");
 const serverless = require("serverless-http");
 const pool = require("./db").pool;
 const bodyParser = require("body-parser");
@@ -7,6 +8,7 @@ const cors = require("cors");
 const api = express();
 const router = express.Router();
 
+api.use(helmet());
 api.use(bodyParser.json());
 api.use(
   cors({
@@ -18,6 +20,34 @@ api.use(
 //Test
 router.get("/", (req, res) => {
   res.status(200).send("Hello");
+});
+
+//get Scores
+router.get("/score/:limit", (req, res) => {
+  let sql;
+  const limit = req.params.limit;
+
+  if (limit === "all" || isNaN(limit)) {
+    sql = "SELECT * FROM highscores ORDER BY score DESC, createdAt ASC)";
+  } else {
+    sql = `SELECT * FROM highscores ORDER BY score DESC, createdAt ASC LIMIT ${limit}`;
+  }
+
+  try {
+    pool.getConnection((err, connection) => {
+      if (err) throw err;
+
+      connection.query(sql, (err, result) => {
+        if (err) throw err;
+
+        res.status(200).json({ scores: result });
+        connection.release();
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err });
+  }
 });
 
 api.use("/api/", router);
