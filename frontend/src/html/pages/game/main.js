1750c74d611589a3a85d95cc6da02c8ca5966ddc
Stefan Turner <turner.stefan281@gmail.com>
2023-09-30 12:49:56 +0200

    add check for new questions

--- frontend/src/html/pages/game/main.js
+++ frontend/src/html/pages/game/main.js
@@ -1,6 +1,7 @@
 import { FetchQuestions } from "../../../js/tools/FetchQuestions";
 import { RenderRound } from "../../../js/tools/RenderRound";
 
+const getQuestionsNum = 5;
 const player = {
   round: 0,
   score: 0,
@@ -10,18 +11,23 @@ const player = {
 
 (async () => {
   PrepareRound();
+})();
 
+async function PrepareRound() {
+  //get new questions from API if no questions in player object or all played
+  if (player.questions === null || player.round % getQuestionsNum === 0) {
+    const questions = await FetchQuestions(getQuestionsNum);
+    player.questions = questions;
+    console.log("newwwwww");
+  }
+
+  RenderRound(player);
+
+  //hide get ready overlay and start timeranimation
   setTimeout(() => {
     document.querySelector(".prep").classList.add("prep--hide");
     document
       .querySelector(".timer__progress")
       .classList.add("timer__progress--play");
   }, 3000);
-})();
-
-async function PrepareRound() {
-  const questions = await FetchQuestions(5);
-  player.questions = questions;
-
-  RenderRound(player);
 }
