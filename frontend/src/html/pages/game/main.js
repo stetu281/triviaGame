9a33b6e26eeea5a5432e5f331cd22d61bb180c26
Stefan Turner <turner.stefan281@gmail.com>
2023-10-21 13:07:56 +0200

    add generateScoreboard function

--- frontend/src/html/pages/game/main.js
+++ frontend/src/html/pages/game/main.js
@@ -155,6 +155,7 @@ function gameOver() {
 
       if (scores) {
         calculateRank(scores, formData);
+        generateScoreboard(scores);
       }
     });
 
@@ -184,4 +185,40 @@ function calculateRank({ scores }, data) {
     scores.length
   } players</p>
 `;
+}
+
+function generateScoreboard({ scores }) {
+  console.log(scores);
+  const top = document.querySelector(".sb__top");
+  const list = document.querySelector(".sb__list");
+
+  if (scores.length < 1) {
+    top.innerText = "Scoreboard empty";
+  } else {
+    for (const [index, item] of scores.entries()) {
+      const container = document.createElement("div");
+
+      if (index >= 0 && index <= 2) {
+        container.classList.add("sb__top3", `sb__top3--${index}`);
+        container.innerHTML = `
+          <img src="../assets/avatar-${item.avatar}-2x.png" alt="" />
+          <p class="sb__top--rank">${index + 1}</p>
+          <p class="sb__top--name">${item.username}</p>
+          <p class="sb__top--points">${item.score}</p>
+        `;
+        top.appendChild(container);
+      } else {
+        container.classList.add("sb__item");
+        container.innerHTML = `
+          <p class="sb__item--rank">${index + 1}</p>
+          <div class="sb__item--container">
+            <img src="../assets/avatar-${item.avatar}-2x.png" />
+            <p class="sb__item--name">${item.username}</p>
+            <p class="sb__item--points">${item.score} P</p>
+          </div>
+        `;
+        list.appendChild(container);
+      }
+    }
+  }
 }
