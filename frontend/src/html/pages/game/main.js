9ce1f9332851cf5eb2016021adf8bf940702f3ff
Stefan Turner <turner.stefan281@gmail.com>
2023-10-21 13:51:16 +0200

    add styling

--- frontend/src/html/pages/game/main.js
+++ frontend/src/html/pages/game/main.js
@@ -5,7 +5,7 @@ import { PostData } from "../../../js/tools/PostData";
 const getQuestionsNum = 5;
 const player = {
   round: 0,
-  score: 100,
+  score: 1000,
   lives: 0,
   questions: null,
 };
@@ -182,11 +182,19 @@ function calculateRank({ scores }, data) {
   const rank = scores.findIndex((score) => score.username === data.username);
 
   document.querySelector(".submitScore").innerHTML = `
-  <img src="../assets/avatar-${data.avatar}-1x.png" alt="" />
-  <p>Thanks ${data.username}</p>
-  <p>Your rank is<br /><span>#${rank + 1}</span><br />of ${
-    scores.length
-  } players</p>
+  <img src="../assets/avatar-${
+    data.avatar
+  }-1x.png" class="submitScore__avatar" alt="" />
+  
+  <p class="submitScore__thanks">Thanks ${data.username}</p>
+  ${
+    rank < 4
+      ? "<p class='submitScore__congrats'>Congratulation, you made the top 3</p>"
+      : ""
+  }
+  <p class="submitScore__rank">You ranked<br /><span>#${
+    rank + 1
+  }</span><br />of ${scores.length} players</p>
 `;
 }
 
